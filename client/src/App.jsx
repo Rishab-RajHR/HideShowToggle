b38138c9f9274a7b64056247aa74d42b27772393
Rishab Raj <rr0948555@gmail.com>
2025-12-02 14:49:26 +0530

    HideShowToggle

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HideShowToggle from './HideShowToggle'
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is the App fie</h1>
+      <HideShowToggle/>
+    </div>
+  )
+}
+
+export default App
